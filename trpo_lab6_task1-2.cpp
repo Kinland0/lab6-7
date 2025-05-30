09aeb6a9bfa98ea380e04bd5700f9e91f0459f45
Kinland0 <egornikolaev-2017@yandex.ru>
2025-05-30 18:43:59 +0400

    Определение базовых классов иерархии Expression
    Добавлены конкретные реализации Number и Variable, наследующие Expression. Реализованы методы evaluate() (возвращают значение или 0 для переменных) и transform(), делегирующие обработку трансформеру.

--- trpo_lab6_task1-2.cpp
+++ trpo_lab6_task1-2.cpp
@@ -29,3 +29,22 @@ struct Transformer {
     virtual Expression* transformVariable(Variable const*) = 0;
 };
 
+
+struct Number : Expression {
+    Number(double value) : value_(value) {}
+    double value() const { return value_; }
+    double evaluate() const override { return value_; }
+    Expression* transform(Transformer* tr) const override { return tr->transformNumber(this); }
+private:
+    double value_;
+};
+
+struct Variable : Expression {
+    Variable(string const& name) : name_(name) {}
+    string const& name() const { return name_; }
+    double evaluate() const override { return 0.0; }
+    Expression* transform(Transformer* tr) const override { return tr->transformVariable(this); }
+private:
+    string const name_;
+};
+
