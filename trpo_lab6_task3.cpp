3e819534543906de07ea68578fa236995dc5927f
Kinland0 <egornikolaev-2017@yandex.ru>
2025-05-30 19:00:31 +0400

    Реализация классов по конкретным продуктам для различных праздников
    Добавлены конкретные реализации продуктов для трёх праздников:
    Новый год (NYCard, NYPresent, NYCake),
    День рождения (BDCard, BDPresent, BDCake),
    8 марта (March8Card, March8Present, March8FlowerArrangement).
    Каждый класс переопределяет методы базовых абстракций, выводя специфичные сообщения для своих сценариев.

--- trpo_lab6_task3.cpp
+++ trpo_lab6_task3.cpp
@@ -21,3 +21,52 @@ public:
     virtual void describe() const = 0;
     virtual ~Cake() = default;
 };
+
+
+class NYCard : public PostCard {
+public:
+    void display() const override { cout << "Íîâîăîäí˙˙ îňęđűňęŕ\n"; }
+};
+
+class NYPresent : public Present {
+public:
+    void show() const override { cout << "Íîâîăîäíčé ďîäŕđîę\n"; }
+};
+
+class NYCake : public Cake {
+public:
+    void describe() const override { cout << "Íîâîăîäíčé ňîđň\n"; }
+};
+
+// Ęîíęđĺňíűĺ ďđîäóęňű äë˙ Äí˙ Đîćäĺíč˙
+class BDCard : public PostCard {
+public:
+    void display() const override { cout << "Îňęđűňęŕ íŕ ÄĐ\n"; }
+};
+
+class BDPresent : public Present {
+public:
+    void show() const override { cout << "Ďîäŕđîę íŕ ÄĐ\n"; }
+};
+
+class BDCake : public Cake {
+public:
+    void describe() const override { cout << "Ňîđň íŕ ÄĐ\n"; }
+};
+
+// Ęîíęđĺňíűĺ ďđîäóęňű äë˙ 8 Ěŕđňŕ 
+class March8Card : public PostCard {
+public:
+    void display() const override { cout << "Îňęđűňęŕ ę 8 Ěŕđňŕ\n"; }
+};
+
+class March8Present : public Present {
+public:
+    void show() const override { cout << "Ďîäŕđîę íŕ 8 Ěŕđňŕ\n"; }
+};
+
+class March8FlowerArrangement : public Cake { 
+public:
+    void describe() const override { cout << "Áóęĺň öâĺňîâ ę 8 Ěŕđňŕ\n"; }
+};
+
